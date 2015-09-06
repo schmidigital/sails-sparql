3e6988ba053b183768bf023cad24a3a9f7f71f10
smith <schmid@mindbabble.com>
2015-09-06 13:44:26 +0200

    populate funktionen!

--- test/temporary/support/bootstrap.js
+++ test/temporary/support/bootstrap.js
@@ -5,11 +5,27 @@
 var Waterline = require('waterline');
 var _ = require('lodash');
 var async = require('async');
-var assert = require('assert');
 
 // Require Fixtures
 var fixtures = {
-  UserFixture: require('./fixtures/crud.fixture')
+  PaymentBelongsFixture: require('./fixtures/belongsTo.child.fixture'),
+  PaymentBelongsCustomFixture: require('./fixtures/belongsTo.child.customPK.fixture'),
+  CustomerBelongsFixture: require('./fixtures/belongsTo.parent.fixture'),
+  CustomerBelongsCustomFixture: require('./fixtures/belongsTo.parent.customPK.fixture'),
+  PaymentHasManyFixture: require('./fixtures/hasMany.child.fixture'),
+  CustomerHasManyFixture: require('./fixtures/hasMany.parent.fixture'),
+  ApartmentHasManyFixture: require('./fixtures/hasMany.customPK.fixture'),
+  PaymentManyFixture: require('./fixtures/multipleAssociations.fixture').payment,
+  CustomerManyFixture: require('./fixtures/multipleAssociations.fixture').customer,
+  StadiumFixture: require('./fixtures/hasManyThrough.stadium.fixture'),
+  TeamFixture: require('./fixtures/hasManyThrough.team.fixture'),
+  VenueFixture: require('./fixtures/hasManyThrough.venue.fixture'),
+  TaxiFixture: require('./fixtures/manyToMany.taxi.fixture'),
+  DriverFixture: require('./fixtures/manyToMany.driver.fixture'),
+  TaxiCustomFixture: require('./fixtures/manyToMany.taxi.customPK.fixture'),
+  DriverCustomFixture: require('./fixtures/manyToMany.driver.customPK.fixture'),
+  UserOneFixture: require('./fixtures/oneToOne.fixture').user_resource,
+  ProfileOneFixture: require('./fixtures/oneToOne.fixture').profile
 };
 
 
@@ -27,7 +43,7 @@ before(function(done) {
     waterline.loadCollection(fixtures[key]);
   });
 
-  var connections = { queryable: _.clone(Connections.test) };
+  var connections = { associations: _.clone(Connections.test) };
 
   waterline.initialize({ adapters: { wl_tests: Adapter }, connections: connections }, function(err, _ontology) {
     if(err) return done(err);
@@ -36,7 +52,7 @@ before(function(done) {
 
     Object.keys(_ontology.collections).forEach(function(key) {
       var globalName = key.charAt(0).toUpperCase() + key.slice(1);
-      global.Queryable[globalName] = _ontology.collections[key];
+      global.Associations[globalName] = _ontology.collections[key];
     });
 
     done();
